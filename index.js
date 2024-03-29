2add16b5294f1e12752fc748cb04f2e4402b148d
shahina akter shima <shima421914@gmail.com>
2024-03-29 06:47:43 +0600

    data

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const port=process.env.PORT||5000;
 app.use(cors());
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173',
-  "https://assignment-12-client-seven.vercel.app","https://assignment-12-client-1elh923xa-shahinaaktershimas-projects.vercel.app","https://assignment-12-client-git-main-shahinaaktershimas-projects.vercel.app"
+  origin: ['http://localhost:5173',"https://assignment-12-lyart.vercel.app"
+  // "https://assignment-12-client-seven.vercel.app","https://assignment-12-client-1elh923xa-shahinaaktershimas-projects.vercel.app","https://assignment-12-client-git-main-shahinaaktershimas-projects.vercel.app"
   ],
   credentials: true,
 }))
